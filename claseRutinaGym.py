a58bf106208eca54e7429444065549fd865879ac
EvelynHP <evelyn.hernandezpr@uanl.edu.mx>
2026-04-16 20:05:11 -0600

    Se creo clase con atributos

---
+++ claseRutinaGym.py
@@ -0,0 +1,10 @@
+class RutinaGym:
+    def __init__(self, nombre, duracion, calorias, ejercicios, nivel):
+        self.__nombre = nombre
+        self.__duracion = duracion
+        self.__calorias = calorias
+        self.__ejercicios = ejercicios 
+        self.__nivel = nivel 
+
+        
+
